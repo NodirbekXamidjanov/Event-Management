8e2851e4464e0e7d075b22b259b20f1a8e570cad
Nodirbek <xamidjonovnodirbek@gmail.com>
2025-08-06 14:17:27 +0500

    card-class-finalized

--- src/bank-system/card.ts
+++ src/bank-system/card.ts
@@ -3,11 +3,12 @@ import { nowDate } from "./get-date";
 import type { cardType, Deposit, isActive } from "./types";
 
 export class Card {
-  public balance: number;
-  public isActive: isActive;
-  public dailyLimit: number;
-  public monthlyLimit: number;
-  public transactionHistory: Deposit[];
+  balance: number;
+  isActive: isActive;
+  dailyLimit: number;
+  monthlyLimit: number;
+  transactionHistory: Deposit[];
+  transactionId: number;
   constructor(
     public cardNumber: number,
     public expiryDate: string,
@@ -21,10 +22,10 @@ export class Card {
     this.dailyLimit = DAILY_LIMIT;
     this.monthlyLimit = MONTHLY_LIMIT;
     this.transactionHistory = [];
+    this.transactionId = 0;
   }
 
   deposit(amount: number) {
-    let id = 0;
     try {
       if (!this.isActive || amount <= 0) {
         throw new Error("Card is Disactive or incorrect Amount");
@@ -32,21 +33,58 @@ export class Card {
       this.balance += amount;
       this.transactionHistory.push({
         status: "SUCCES",
-        id: id,
+        id: this.transactionId,
         amount: amount,
         date: nowDate,
       });
-      id++;
+      this.transactionId;
       console.log("Deposit is succesfuly");
     } catch (error) {
       console.error((error as Error).message, "Deposit is Failed");
     }
   }
   withdraw(amount: number) {
-    
+    try {
+      if (
+        !this.isActive ||
+        this.balance <= 0 ||
+        this.dailyLimit <= 0 ||
+        amount <= 0
+      ) {
+        throw new Error("Withdrawal Error");
+      }
+      this.balance -= amount;
+      this.transactionHistory.push({
+        status: "SUCCES",
+        id: this.transactionId,
+        amount: amount,
+        date: nowDate,
+      });
+      this.transactionId;
+      console.log("Withdrawal is succesfuly");
+    } catch (error) {
+      console.error((error as Error).message, "withdraw failed");
+    }
   }
-  changePin(newPin: number) {}
-  block() {}
-  activate() {}
-  canSpend(amount: number) {}
+  changePin(newPin: number) {
+    try {
+      if (newPin.toFixed().length !== 4) {
+        throw new Error("The length of the pin code must be 4.");
+      }
+      this.pin = newPin;
+      console.log("Pin updated");
+    } catch (error) {
+      console.error((error as Error).message, "New Pin incorrect");
+    }
+  }
+  block() {
+    this.isActive = "Disactive";
+  }
+  activate() {
+    this.isActive = "Acitve";
+  }
+  canSpend(amount: number) {
+    const can = this.balance - amount;
+    console.log(can ? "canSpend" : "cannotSpend");
+  }
 }
